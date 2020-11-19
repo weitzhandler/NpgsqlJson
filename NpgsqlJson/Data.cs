5b7519be6eec40baf392bc0ab6d7af2d93f0517c
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2020-11-19 18:59:48 +0200

    Added exception commentAdd project files.

---
+++ NpgsqlJson/Data.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace NpgsqlJson
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers => Set<Customer>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Host=127.0.0.1;Database=npgsql_tests;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var customerConfig = modelBuilder.Entity<Customer>();
+
+            customerConfig
+                .Property(customer => customer.Names)
+                .HasColumnType("jsonb");
+
+            customerConfig
+                .Property(customer => customer.Phones)
+                .HasColumnType("jsonb");
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        HashSet<string>? _Names;
+        public ICollection<string> Names
+        {
+            get => _Names ??= new HashSet<string>();
+            set => _Names = value.ToHashSet();
+        }
+
+        HashSet<Phone>? _Phones;
+        public ICollection<Phone> Phones
+        {
+            get => _Phones ??= new HashSet<Phone>();
+            set => _Phones = value?.ToHashSet();
+        }
+    }
+
+    public class Phone
+    {
+        public string? Place { get; set; }
+        public string? Number { get; set; }
+    }
+}
---
+++ NpgsqlJson/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace NpgsqlJson
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task Should_save_customer()
+        {
+            // arrange
+            using var dbContext = new AppDbContext();
+            await dbContext.Database.EnsureCreatedAsync();
+            var customer =
+                new Customer
+                {
+                    Names =
+                    {
+                        "John",
+                        "Charlie"
+                    },
+                    Phones =
+                    {
+                        new Phone
+                        {
+                            Place = "Home",
+                            Number = "123-4567-8910"
+                        },
+                        new Phone
+                        {
+                            Place = "Work",
+                            Number = "123-4567-8910"
+                        }
+                    }
+                };
+            dbContext.Customers.Add(customer);
+
+            // act
+            var result = await dbContext.SaveChangesAsync();
+            dbContext.ChangeTracker.Clear();
+
+
+            /* The following line throws
+             * System.InvalidCastException : Unable to cast object of type 'System.Collections.Generic.List`1[System.String]' to type 'System.Collections.Generic.HashSet`1[System.String]'.
+             * 
+             * StackTrace:
+             * lambda_method(Closure , QueryContext , DbDataReader , ResultContext , SingleQueryResultCoordinator )
+             *    AsyncEnumerator.MoveNextAsync()
+             *    ShapedQueryCompilingExpressionVisitor.SingleOrDefaultAsync[TSource](IAsyncEnumerable`1 asyncEnumerable, CancellationToken cancellationToken)
+             *    ShapedQueryCompilingExpressionVisitor.SingleOrDefaultAsync[TSource](IAsyncEnumerable`1 asyncEnumerable, CancellationToken cancellationToken)
+             */
+
+            var retrieved = await dbContext.Customers.SingleOrDefaultAsync(cust => cust.Id == customer.Id);
+
+            // assert
+            Assert.Equal(1, result);
+            Assert.Equal(retrieved.Id, customer.Id);
+        }
+    }
+}
